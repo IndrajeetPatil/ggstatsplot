fa9f8c13e6bb761d56d7abe16cbaa12ab77c3f5f
IndrajeetPatil <inderonline1988@gmail.com>
2018-01-07 18:57:28 -0500

    created first draft of ggscatterstat function

---
+++ R/ggscatterstat.R
@@ -0,0 +1,157 @@
+#' @title scatterplot with ggMarginals
+#' @name ggscatterstat
+#' @author Indrajeet Patil
+#'
+#' @param df data frame from which variables specified are preferentially to be taken
+#' @param x a vector containing the explanatory variable
+#' @param xlab label for x axis variable
+#' @param ylab label for y axis variable
+#' @param y the response - a vector of length the number of rows of `x`
+#' @param marginal decides whether `ggExtra::ggMarginal()` plots will be displayed; the default is `TRUE`
+#' @param marginaltype type of marginal distribution to be plotted on the axes (NULL, "histogram", "boxplot", "density")
+#' @param xfill color fill for x axis distibution
+#' @param yfill color fill for y axis distribution
+#' @param test statistical test to be run and displayed as subtitle ("parametric", "nonparametric", "robust")
+#' @param intercept decides whether mean intercept lines are to be plotted
+#' @param title title for the plot
+#' @param caption caption for the plot
+#' @export
+
+library(ggplot2)
+library(grid)
+library(ggExtra)
+library(stats)
+library(MASS)
+library(sfsmisc)
+
+ggscatterstat <- function(df, x, y, xlab = NULL, ylab = NULL, marginal = NULL,
+                          marginaltype = NULL, xfill = NULL, yfill = NULL, intercept = NULL,
+                          test = NULL, title = NULL, caption = NULL) {
+
+  ### custom function for getting specified number of decimal places in results
+  # x is a numeric value, while k is the number of digits after decimal point (should be an integer)
+
+  specify_decimal <- function(x, k = NULL) {
+
+    # if the number of decimal places hasn't been specified, use the default of 3
+    if (is.null(k)) k <- 3
+    output <- trimws(format(round(x, k), nsmall = k))
+    #if (output < 0.001) output <- "< 0.001"
+    return(output)
+
+  }
+
+  ### custom function for getting specified number of decimal places in results for p-value
+  # x is a numeric value, while k is the number of digits after decimal point (should be an integer)
+
+  specify_decimal_p <- function(x, k = NULL) {
+
+    # if the number of decimal places hasn't been specified, use the default of 3
+    if (is.null(k)) k <- 3
+    output <- trimws(format(round(x, k), nsmall = k))
+    if (output < 0.001) output <- "< 0.001"
+    return(output)
+
+  }
+
+  ## default theme to use for ggplot figures
+
+  theme_mprl <- function() {
+
+    ggplot2::theme_grey() +
+      ggplot2::theme(axis.title.x = element_text(size = 14, face = "bold"),
+                     strip.text.x = element_text(size = 14, face = "bold"),
+                     strip.text.y = element_text(size = 14, face = "bold"),
+                     strip.text = element_text(size = 14, face = "bold"),
+                     axis.title.y = element_text(size = 14, face = "bold"),
+                     axis.text.x = element_text(size = 14, face = "bold"),
+                     axis.text.y = element_text(size = 14, face = "bold"),
+                     axis.line = element_line(),
+                     legend.text = element_text(size = 14),
+                     legend.title = element_text(size = 14, face = "bold"),
+                     legend.title.align = 0.5,
+                     legend.text.align = 0.5,
+                     legend.key.height = unit(1,"line"),
+                     legend.key.width = unit(1,"line"),
+                     plot.margin = unit(c(1,1,1,1), "lines"), # requires library(grid))
+                     panel.border = element_rect(colour = "black", fill = NA, size = 1),
+                     plot.title = element_text(color = "black", size = 16, face = "bold", hjust = 0.5),
+                     plot.subtitle = element_text(color = "black", size = 12, face = "bold", hjust = 0.5))
+
+  }
+
+
+  # if fill colors for x and y axes are not specified, use the defaults
+  if (is.null(xfill)) xfill <- "orange"
+  if (is.null(yfill)) yfill <- "green"
+
+  # if test to be run is not satisfied, then use default, which is robust regression from MASS package
+  if (is.null(test)) test <- "robust"
+
+  if (test == "parametric") {
+
+    # running the correlation test and preparing the subtitle text
+    c <- stats::cor.test(x, y, method = "parametric", exact = FALSE)
+    corr_label <- base::substitute(paste("Pearson's ", italic("r"), "(", df, ")", " = ", estimate, ", ", italic("p")," = ", pvalue),
+                                   list(df = specify_decimal(c$parameter, 0),
+                                        estimate = specify_decimal(c$estimate, 3),
+                                        pvalue = specify_decimal_p(c$p.value, 3)))
+
+  }   else if (test == "nonparametric") {
+
+    # running the correlation test and preparing the subtitle text
+    c <- stats::cor.test(x, y, method = "nonparametric", exact = FALSE)
+    corr_label <- base::substitute(paste("Spearman's ", italic(rho), "(", df, ")", " = ", estimate, ", ", italic("p")," = ", pvalue),
+                                   list(df = specify_decimal(c$parameter, 0),
+                                        estimate = specify_decimal(c$estimate, 3),
+                                        pvalue = specify_decimal_p(c$p.value, 3)))
+
+  } else if (test == "robust") {
+
+    # running robust regression test and preparing the subtitle text
+    MASS_res <- MASS::rlm(scale(y) ~ scale(x), maxit = 1000, na.action = na.omit, data = df)
+    corr_label <- base::substitute(paste("robust regression: estimate = ", estimate, ", ", italic("t"),
+                                         "(", df, ")", " = ", t, ", ", italic("p")," = ", pvalue),
+                                   list(estimate = specify_decimal(summary( MASS_res)$coefficients[[2]], 3),
+                                        t = specify_decimal(summary( MASS_res)$coefficients[[6]], 3),
+                                        df = summary( MASS_res)$df[2],
+                                        pvalue = specify_decimal_p((sfsmisc::f.robftest( MASS_res))$p.value),3))
+  }
+
+  # preparing the scatterplotplot
+  plot <- ggplot2::ggplot(df, aes(x = x, y = y)) +
+    geom_count(show.legend = FALSE, color = "black", size = 3, alpha = 0.5,
+               position = position_jitterdodge(jitter.width = NULL, jitter.height = 0.2, dodge.width = 0.75)) +
+    geom_smooth(method = "rlm", se = TRUE, size = 1.5) + # default is robust linear model
+    theme_mprl() + # theme_mprl() is already defined above
+    labs(x = xlab, y = ylab, title = title, subtitle = corr_label, caption = caption)
+
+  # by default intercept lines will be plotted
+  if (is.null(intercept)) intercept <- TRUE
+
+  if (isTRUE(intercept)) {
+
+    plot <- plot +
+      geom_vline(xintercept = mean(x), linetype = "dashed", color = xfill, size = 1.2) +
+      geom_hline(yintercept = mean(y), linetype = "dashed", color = yfill, size = 1.2)
+
+  }
+
+  # if marginal should be plotted or not is not specified, it will be plotted by default
+  if (is.null(marginal)) marginal <- TRUE
+
+  if (isTRUE(marginal)) {
+
+    # if ggmarginal marginaltype has not been specified, go to this default
+    if (is.null(marginaltype)) marginaltype <- "histogram"
+
+    # creating the ggMarginal plot of a given marginaltype
+    plot <- ggExtra::ggMarginal(plot, type = marginaltype, xparams = list(fill = xfill, col = "black"),
+                                yparams = list(fill = yfill, col = "black"))
+  }
+
+  return(plot)
+
+}
+
+
---
+++ man/hello.Rd
@@ -0,0 +1,12 @@
+\name{hello}
+\alias{hello}
+\title{Hello, World!}
+\usage{
+hello()
+}
+\description{
+Prints 'Hello, world!'.
+}
+\examples{
+hello()
+}
