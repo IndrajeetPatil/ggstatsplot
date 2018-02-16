17995499c02a518303865be38edce7e23b8d3808
IndrajeetPatil <inderonline1988@gmail.com>
2018-02-15 22:19:13 -0500

    created new ggstatsplot file with information about the entire package

---
+++ R/ggstatsplot.R
@@ -0,0 +1,22 @@
+#' ggstatsplot
+#'
+#' Collection of functions to enhance \code{ggplot2} with results from statistical tests.
+#' The main functions are-
+#' \itemize{
+#'   \item \code{\link[ggstatsplot]{ggbetweenstats}} function to produce information-rich comparison plot between
+#'   different groups or conditions with `ggplot2` and details from the statistical tests in the subtitle
+#'   \item \code{\link[ggstatsplot]{ggscatterstats}} function to produce `ggplot2` scatterplots along
+#'   with a marginal histograms/boxplots/density plots from `ggExtra` and details from the statistical tests in the subtitle
+#'   \item \code{\link[ggstatsplot]{ggpiestats}} function to produce pie chart with details from
+#'   the statistical tests in the subtitle
+#'   \item \code{\link[ggstatsplot]{theme_mprl}} default theme used for the `ggstatplot` package
+#'   \item \code{\link[ggstatsplot]{specify_decimal_p}} format results for pretty printing
+#' }
+#'
+#' For more documentation, see \href{https://github.com/IndrajeetPatil/ggstatsplot}{README} on GitHub.
+#' Vignette preparation in progress.
+#'
+#' @docType package
+#' @name ggstatsplot
+#'
+NULL
