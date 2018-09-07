d9c353d590df0e5a530df78e688033be14a611b5
Indrajeet Patil <inderonline1988@gmail.com>
2018-09-07 19:15:55 -0400

    praying to old and new gods that travis build will work now

--- R/ggstatsplot-package.R
+++ R/ggstatsplot-package.R
@@ -10,26 +10,31 @@
 #'   nonparametric, and robust versions of t-test/anova, correlation, and
 #'   contingency tables analyses).
 #'
-#' @details The main functions are- \itemize{ \item
-#'   \code{\link[ggstatsplot]{ggbetweenstats}} function to produce
+#' @details The main functions are-
+#' \itemize{
+#'   \item \code{\link[ggstatsplot]{ggbetweenstats}} function to produce
 #'   information-rich comparison plot between different groups or conditions
-#'   with `ggplot2` and details from the statistical tests in the subtitle \item
-#'   \code{\link[ggstatsplot]{ggscatterstats}} function to produce `ggplot2`
+#'   with `ggplot2` and details from the statistical tests in the subtitle.
+#'   \item \code{\link[ggstatsplot]{ggscatterstats}} function to produce `ggplot2`
 #'   scatterplots along with a marginal histograms/boxplots/density plots from
-#'   `ggExtra` and details from the statistical tests in the subtitle \item
-#'   \code{\link[ggstatsplot]{ggpiestats}} function to produce pie chart with
-#'   details from the statistical tests in the subtitle \item
-#'   \code{\link[ggstatsplot]{gghistostats}} function to produce histogram for a
-#'   single variable with results from one sample test displayed in the subtitle
+#'   `ggExtra` and details from the statistical tests in the subtitle.
+#'   \item \code{\link[ggstatsplot]{ggpiestats}} function to produce pie chart with
+#'   details from the statistical tests in the subtitle.
+#'   \item \code{\link[ggstatsplot]{gghistostats}} function to produce histogram for a
+#'   single variable with results from one sample test displayed in the subtitle.
 #'   \item \code{\link[ggstatsplot]{ggcorrmat}} function to visualize
-#'   correlation matrix \item \code{\link[ggstatsplot]{combine_plots}} helper
+#'   correlation matrix.
+#'   \item \code{\link[ggstatsplot]{combine_plots}} helper
 #'   function to combine multiple `ggstatsplot` plots using
 #'   `cowplot::plot_grid()` with a combination of title, caption, and annotation
-#'   label \item \code{\link[ggstatsplot]{ggcoefstats}} function to visualize
-#'   results from regression analyses \item
-#'   \code{\link[ggstatsplot]{theme_ggstatsplot}} default theme used for this
-#'   package \item \code{\link[ggstatsplot]{specify_decimal_p}} helper function
-#'   to format results for pretty printing }
+#'   label.
+#'   \item \code{\link[ggstatsplot]{ggcoefstats}} function to visualize
+#'   results from regression analyses.
+#'   \item \code{\link[ggstatsplot]{theme_ggstatsplot}} default theme used for this
+#'   package.
+#'   \item \code{\link[ggstatsplot]{specify_decimal_p}} helper function
+#'   to format results for pretty printing.
+#'  }
 #'
 #' For more documentation, see the dedicated
 #' \href{https://indrajeetpatil.github.io/ggstatsplot/}{Website}.
@@ -37,4 +42,4 @@
 #' @docType package
 #' @name ggstatsplot-package
 #'
-NULL
+"_PACKAGE"
