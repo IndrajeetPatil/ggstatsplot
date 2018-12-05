c6a9fc8787e83f09750fc43725b2582298932eae
Chuck Powell <ibecav@gmail.com>
2018-12-05 13:41:23 -0500

    test_kw_eta_h_ci added

---
+++ tests/testthat/test_kw_eta_h_ci.R
@@ -0,0 +1,25 @@
+context("test_kw_eta_h_ci")
+
+testthat::test_that(
+  desc = "Confidence interval for effect size for Kruskal-Wallis test",
+  code = {
+
+    # function output
+    set.seed(123)
+    using_function1 <-
+      suppressWarnings(ggstatsplot:::kw_eta_h_ci(
+        data = movies_long,
+        genre,
+        budget,
+        nboot = 50,
+        conf.level = 0.999
+      ))
+
+
+    # testing 4 conditions
+    testthat::expect_equal(using_function1$eta_sq_H, 0.0765, tolerance = 0.0001)
+    testthat::expect_equal(using_function1$conf.low, 0.0365, tolerance = 0.0001)
+    testthat::expect_equal(using_function1$conf.high, 0.110, tolerance = 0.0001)
+    testthat::expect_equal(using_function1$nboot, 50)
+  }
+)
