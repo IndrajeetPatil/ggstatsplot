690268df0d08ae92209b812376b7dd507b6bf824
Indrajeet Patil <inderonline1988@gmail.com>
2019-02-23 00:03:44 -0500

    more tests for ggwithinstats

--- tests/testthat/test_ggwithinstats.R
+++ tests/testthat/test_ggwithinstats.R
@@ -7,40 +7,60 @@ testthat::test_that(
   code = {
     set.seed(123)
 
+    p0 <- ggstatsplot:::ggwithinstats(
+      data = ggstatsplot::iris_long,
+      x = condition,
+      y = value,
+      type = "p",
+      outlier.tagging = TRUE,
+      pairwise.comparisons = TRUE,
+      messages = TRUE
+    )
+
     p1 <- ggstatsplot:::ggwithinstats(
       data = ggstatsplot::iris_long,
       x = condition,
       y = value,
-      type = "bf",
+      type = "p",
+      pairwise.display = "ns",
       outlier.tagging = TRUE,
       pairwise.comparisons = TRUE,
-      messages = TRUE
+      messages = FALSE
     )
 
     p2 <- ggstatsplot:::ggwithinstats(
       data = ggstatsplot::iris_long,
       x = condition,
       y = value,
+      type = "bf",
       outlier.tagging = TRUE,
       pairwise.comparisons = TRUE,
-      pairwise.display = "ns",
-      pairwise.annotation = "p",
-      bf.message = TRUE,
-      messages = TRUE
+      messages = FALSE
     )
 
     p3 <- ggstatsplot:::ggwithinstats(
       data = ggstatsplot::iris_long,
+      x = condition,
+      y = value,
+      outlier.tagging = TRUE,
+      pairwise.comparisons = TRUE,
+      pairwise.annotation = "p",
+      bf.message = TRUE,
+      messages = FALSE
+    )
+
+    p4 <- ggstatsplot:::ggwithinstats(
+      data = ggstatsplot::iris_long,
       x = attribute,
       y = value,
       pairwise.comparisons = TRUE,
-      pairwise.display = "s",
+      pairwise.display = "ns",
       pairwise.annotation = "asterisk",
       messages = FALSE,
       bf.message = TRUE
     )
 
-    p4 <- ggstatsplot:::ggwithinstats(
+    p5 <- ggstatsplot:::ggwithinstats(
       data = ggstatsplot::iris_long,
       x = attribute,
       y = value,
@@ -50,18 +70,22 @@ testthat::test_that(
       messages = FALSE
     )
 
-    p5 <- ggstatsplot:::ggwithinstats(
+    p6 <- ggstatsplot:::ggwithinstats(
       data = ggstatsplot::iris_long,
       x = attribute,
       y = value,
+      axes.range.restrict = TRUE,
       outlier.tagging = TRUE,
       outlier.label = condition,
       messages = FALSE
     )
 
+    testthat::expect_is(p0, "ggplot")
     testthat::expect_is(p1, "ggplot")
     testthat::expect_is(p2, "ggplot")
     testthat::expect_is(p3, "ggplot")
     testthat::expect_is(p4, "ggplot")
+    testthat::expect_is(p5, "ggplot")
+    testthat::expect_is(p6, "ggplot")
   }
 )
