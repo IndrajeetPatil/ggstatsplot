374c508d9cbfb1adeb340ab4ba126f32fcc56185
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-11-24 16:40:58 +0100

    DRY vignette setup

---
+++ vignettes/setup.R
@@ -0,0 +1,23 @@
+knitr::opts_chunk$set(
+  collapse  = TRUE,
+  comment   = "#>",
+  warning   = FALSE,
+  message   = FALSE,
+  out.width = "100%",
+  dpi       = 300
+)
+
+# to pretty-print all columns in the output tibble
+options(
+  tibble.width      = Inf,
+  pillar.bold       = TRUE,
+  pillar.neg        = TRUE,
+  pillar.subtle_num = TRUE,
+  pillar.min_chars  = Inf
+)
+
+# local library
+library(ggstatsplot)
+
+# for reproducibility
+set.seed(123)
