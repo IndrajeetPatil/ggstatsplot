54b373f73dd8807a4a58aa70ce35486ca6a95568
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-05-05 09:17:59 +0200

    Update snapshots using R 4.4.0 (#930)
    * Update snapshots using R 4.4.0
    
    Closes #872
    
    * merge strict and standard rcmdcheck workflows
    
    * Update R-CMD-check.yaml
    
    * reinstall Matrix
    
    * Update R-CMD-check.yaml
    
    * Update R-CMD-check.yaml
    
    * throw everything but the kitchen sink
    
    * Update R-CMD-check.yaml

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,6 @@
 # graphics engine changed in this version, and so snapshots generated on
 # previous R version won't work
-if (getRversion() < "4.4.0") {
+if (getRversion() >= "4.4.0") {
   library(testthat)
   suppressPackageStartupMessages(library(ggstatsplot))
 
