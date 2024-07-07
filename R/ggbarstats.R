5c6ca1a2a377ef8a0a52248a031c88cada5c550a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-07-07 18:22:10 +0200

    Ensure `ratio` arg is used in two-way crosstabs

--- R/ggbarstats.R
+++ R/ggbarstats.R
@@ -121,7 +121,7 @@ ggbarstats <- function(
   descriptive_df <- descriptive_data(data, {{ x }}, {{ y }}, label, digits.perc)
 
   # data frame containing all details needed for prop test
-  onesample_df <- onesample_data(data, {{ x }}, {{ y }}, digits)
+  onesample_df <- onesample_data(data, {{ x }}, {{ y }}, digits, ratio)
 
   # if no. of factor levels is greater than the default palette color count
   .is_palette_sufficient(package, palette, nlevels(pull(data, {{ x }})))
