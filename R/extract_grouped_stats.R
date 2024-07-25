e27213af26e632a2c80cc2919a1b99aa8542960c
oranwutang <gzapatawainberg@gmail.com>
2024-07-26 00:10:57 +0200

    creating a function for easily extract stats for all the plots in a grouped plot

--- R/extract_grouped_stats.R
+++ R/extract_grouped_stats.R
@@ -1,16 +1,24 @@
+#' Get stats from grouped plots
+#'
+#' @param p A grouped plot made with grouped_ggwithinstats()
+#'
+#' @return A tibble summarising stats for a grouped plot made with ggstastplot
+#' @export
+#'
+#'
 extract_grouped_stats <- function(p = NULL) {
   stats_0 <- NULL
   for (plot_i in 1:NROW(p)) {
     Group <- p[[plot_i]]$labels$title
     table_i <- extract_stats(p[[plot_i]])$subtitle_data
 # Column 'expression' is a list, in order to make the output compatible
-# with savinv functions as openxlsx::save_xlsx it must be converted to
+# with exporting functions as openxlsx::save_xlsx it must be converted to
 # a vector of chars
     table_i$expression <- as.character(table_i$expression)
     stats_i <- data.frame(Group, table_i)
     stats_0 <- rbind(stats_0, stats_i)
   }
-  stats_0
+  tibble::tibble(stats_0)
 }
 
 
