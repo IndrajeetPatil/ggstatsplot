a35a7ef9023ed54c9d4fab6a25ff03bd1a1e4a20
Indrajeet Patil <patilindrajeet.science@gmail.com>
2026-04-18 18:18:21 +0200

    test: add subject.id to ggwithinstats test cases

--- tests/testthat/test-ggwithinstats.R
+++ tests/testthat/test-ggwithinstats.R
@@ -15,15 +15,16 @@ test_that(
     expect_doppelganger(
       title = "defaults plots - two groups",
       fig = ggwithinstats(
-        data = data_bugs_2,
-        x = condition,
-        y = desire,
+        data           = data_bugs_2,
+        x              = condition,
+        y              = desire,
+        subject.id     = subject,
         pairwise.display = "none",
-        ggsignif.args = list(textsize = 6, tip_length = 0.01),
+        ggsignif.args  = list(textsize = 6, tip_length = 0.01),
         point.path.args = list(color = "red"),
         centrality.path.args = list(color = "blue", size = 2, alpha = 0.8),
         centrality.point.args = list(size = 3, color = "darkgreen", alpha = 0.5),
-        title = "bugs dataset"
+        title          = "bugs dataset"
       )
     )
 
@@ -31,11 +32,12 @@ test_that(
     expect_doppelganger(
       title = "defaults plots - more than two groups",
       fig = ggwithinstats(
-        data = WRS2::WineTasting,
-        x = Wine,
-        y = Taste,
+        data       = WRS2::WineTasting,
+        x          = Wine,
+        y          = Taste,
+        subject.id = Taster,
         pairwise.display = "none",
-        title = "wine tasting data"
+        title      = "wine tasting data"
       )
     )
   }
@@ -50,9 +52,10 @@ test_that(
     expect_doppelganger(
       title = "ggplot2 commands work",
       fig = ggwithinstats(
-        data = WRS2::WineTasting,
-        x = Wine,
-        y = Taste,
+        data             = WRS2::WineTasting,
+        x                = Wine,
+        y                = Taste,
+        subject.id       = Taster,
         results.subtitle = FALSE,
         pairwise.display = "none",
         ggplot.component = ggplot2::labs(y = "Taste rating")
@@ -66,10 +69,11 @@ test_that(
         iris_long,
         condition,
         value,
+        subject.id            = id,
         centrality.point.args = list(size = 5, alpha = 0.5, color = "darkred"),
-        centrality.path = FALSE,
-        results.subtitle = FALSE,
-        pairwise.display = "none"
+        centrality.path       = FALSE,
+        results.subtitle      = FALSE,
+        pairwise.display      = "none"
       )
     )
   }
@@ -87,6 +91,7 @@ test_that(
         data         = filter(bugs_long, condition %in% c("HDHF", "HDLF")),
         x            = condition,
         y            = desire,
+        subject.id   = subject,
         grouping.var = gender,
         type         = "np"
       )
